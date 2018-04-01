91b9fbc961c2f82ad40cfbfbcf01d9d8804abbcd
dingdang <dingdang2018.abc@outlook.com>
2018-04-01 09:53:49 +0800

    初始化，在屏幕上方画飞船，之后窗口会立即关闭

---
+++ youxi.py
@@ -0,0 +1,15 @@
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((1000, 400))
+
+feichuan_image = pygame.image.load('jiantou.bmp')
+feichuan_rect = feichuan_image.get_rect()
+feichuan_rect.centerx = 270
+
+print(feichuan_rect)
+
+screen.blit(feichuan_image, feichuan_rect)
+
+pygame.display.flip()
